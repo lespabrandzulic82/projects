0417e221f2be3336a18737ceccdae9c895a932f4
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-02-04 17:30:07 +0100

    add commit

--- hello-world/helloworld/Main.java
+++ hello-world/helloworld/Main.java
@@ -3,8 +3,8 @@ package helloworld;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
+		System.out.println("My name is Branko!");
 	}
 
 }
