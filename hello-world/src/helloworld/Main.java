3957f9ceece87fd23d00a07f5d00929dfd7a4a03
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-02-06 23:59:16 +0100

    add

--- hello-world/src/helloworld/Main.java
+++ hello-world/src/helloworld/Main.java
@@ -1,10 +1,11 @@
 package helloworld;
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
+		System.out.println("My name is Branko.");
 	}
 
 }
