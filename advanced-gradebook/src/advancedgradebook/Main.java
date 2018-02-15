b79876b725ceecb8960a41e94e5e5b79c7d05897
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-02-15 13:04:38 +0100

    add commit

--- advanced-gradebook/src/advancedgradebook/Main.java
+++ advanced-gradebook/src/advancedgradebook/Main.java
@@ -1,10 +1,46 @@
 package advancedgradebook;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String [][] students = null;
+		int [] grades = null;
+		int numbStudents;
+		double totalGrade = 0;
+		double averageGrade = 0;
+		
+		try (Scanner sc = new Scanner(System.in)) {
 
+			System.out.println("Enter number of students in your class");
+			numbStudents = sc.nextInt();
+			
+			students = new String[numbStudents][2];
+			grades = new int[numbStudents];
+			for(int i = 0; i < numbStudents; i++) {
+				System.out.println("Enter student name");
+				students[i][0] = sc.next();
+				System.out.println("Enter student surname");
+				students[i][1] = sc.next();
+				System.out.println("Enter student grade");
+				grades[i] = sc.nextInt();
+				
+			}
+			
+			for(int t = 0; t < numbStudents; t++) {
+				System.out.println("Student " + students[t][0] + " " + students[t][1] + " has grade " + grades[t] + "." );
+				totalGrade += grades[t];
+				
+				
+			}
+				averageGrade = totalGrade/numbStudents; 
+				System.out.println("Total grade of all students is " + totalGrade + " and average is " + averageGrade + ".");
+			
+			 
+			
+		}
+		
 	}
 
 }
