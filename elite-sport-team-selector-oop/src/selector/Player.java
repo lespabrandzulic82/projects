abd02b2c3611e0de202e9dd7ddc3e4f5464f8131
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-02-24 23:16:33 +0100

    add

--- elite-sport-team-selector-oop/src/selector/Player.java
+++ elite-sport-team-selector-oop/src/selector/Player.java
@@ -1,5 +1,63 @@
 package selector;
 
 public class Player {
-
+	
+	// Attributes  for class Player
+	
+	private String name;
+	private String surname;
+	private int height;
+	private int age;
+	private double points;
+	private double assistances;
+	private int jumps;
+	private int position;
+	
+	
+	//constructor for class Player
+	
+	public Player(String name, String surname, int height, int age, double points, double assistances, int jumps, int position) {
+		this.name = name;
+		this.surname = surname;
+		this.height = height;
+		this.age = age;
+		this.points = points;
+		this.assistances = assistances;
+		this.jumps = jumps;
+		this.position = position;
+	}
+	
+	// methods for class Player
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getPoints() {
+		return points;
+	}
+	
+	public double getAssistances() {
+		return assistances;
+	}
+	
+	public int getJumps() {
+		return jumps;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
 }
