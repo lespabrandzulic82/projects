21e6f6d92129a565b6343d68d4e02e83ae9522bf
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-02-24 14:21:33 +0100

    add commit

--- elite-sport-team-selector/src/selector/Main.java
+++ elite-sport-team-selector/src/selector/Main.java
@@ -13,7 +13,12 @@ public class Main {
 		 System.out.println("Position 5 - center.");
 		 System.out.println("Please submit your application.");
 		 
-		 try (Scanner sc= new Scanner(System.in)){
+		 playerDemo();
+		 
+}
+		private static void playerDemo() {
+								
+			try (Scanner sc = new Scanner(System.in)){
 			 
 			  System.out.println("Please enter your name:");
 			  String name = sc.next();
@@ -26,29 +31,32 @@ public class Main {
 			  System.out.println("What is your average score per game in last season:");
 			  double points = sc.nextDouble();
 			  System.out.println("What is your average namber of assistentions per game in last season:");
-			  double asist = sc.nextDouble();
+			  double assistances = sc.nextDouble();
 			  System.out.println("What is your average number of jumps per game in last season:");
-			  double jump = sc.nextDouble();
+			  int jumps = sc.nextInt();
 			  
 			  System.out.println("Please choose your position: 1,3,5 ");
 			  int position = sc.nextInt();
 			  
-			  if (position == 1 && age > 25 && height < 200 && asist > 6) {
-				  System.out.println("Thank you " + name + " " + surname + " for applying for PLAY position.");
+			  Player player = new Player(name,  surname,  height,  age,  points,  assistances, jumps,  position);
+			  
+			  if (position == 1 && age > 25 && height < 200 && assistances > 6) {
+				  System.out.println("Thank you " + player.getName() + " " + player.getSurname() + " for applying for PLAY position.");
+				  System.out.println("Your charateristics is: height - " + player.getHeight() + "cm, years - " + player.getAge() + ", points - " + player.getPoints() + " per game, assistances - " + player.getAssistances() + " per game and jumps - " + player.getJumps() + " per game.");
 				  System.out.println("Your application has been accepted, we will contact you soon!");
-			  } else if (position == 3 && age > 20 && height > 190 && points > 12) {
-				  System.out.println("Thank you " + name + " " + surname + " for applying for SMALL FORWARD position.");
+			  } else if (position == 3 && player.getAge() > 20 && player.getHeight() > 190 && player.getPoints() > 12) {
+				  System.out.println("Thank you " + player.getName() + " " + player.getSurname() + " for applying for SMALL FORWARD position.");
+				  System.out.println("Your charateristics is: height - " + player.getHeight() + "cm, years - " + player.getAge() + ", points - " + player.getPoints() + " per game, assistances - " + player.getAssistances() + " per game and jumps - " + player.getJumps() + " per game.");
 				  System.out.println("Your application has been accepted, we will contact you soon!");
-			  } else if (position == 5  && age > 28 && height > 205 && jump > 7) {
-				  System.out.println("Thank you " + name + " " + surname + " for applying for CENTER position.");
+			  } else if (position == 5  && player.getAge() > 28 && player.getHeight() > 205 && player.getJumps() > 7) {
+				  System.out.println("Thank you " + player.getName() + " " + player.getSurname() + " for applying for CENTER position.");
+				  System.out.println("Your charateristics is: height - " + player.getHeight() + "cm, years - " + player.getAge() + ", points - " + player.getPoints() + " per game, assistances - " + player.getAssistances() + " per game and jumps - " + player.getJumps() + " per game.");
 				  System.out.println("Your application has been accepted, we will contact you soon!");
 			  } else {
-				  System.out.println("Thank you " + name + " " + surname + " for applying.");
-				  System.out.println("Unfortunatelly, your characteristics are not matching our criteria!");
+				  System.out.println("Thank you " + player.getName() + " " + player.getSurname() + " for applying.");
+				  System.out.println("Your charateristics is: height - " + player.getHeight() + "cm, years - " + player.getAge() + ", points - " + player.getPoints() + " per game, assistances - " + player.getAssistances() + " per game and jumps - " + player.getJumps() + " per game.");
+				  System.out.println("Unfortunately, your characteristics are not matching our criteria!");
 			  }
-	
-		}
-		
-	}
-
+		 }
+	 }
 }
