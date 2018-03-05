2dbfecd5a2641bf07511dd15adfb1eac62e2872b
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-03-05 15:12:29 +0100

    add commit

--- bank-loans/src/bankloans/Main.java
+++ bank-loans/src/bankloans/Main.java
@@ -11,53 +11,70 @@ public class Main {
 		System.out.println("Select the kind of credit you want to get information about");
 				
 		clientDemo();
+		
+		
 	}
 	
+	
 	private static void clientDemo() {
+	
 		
 		try(Scanner sc = new Scanner(System.in)){
-			
+			while(true) {
+					
+				
+				int i = 0;
+				String[] names = new String[i];
+				String[] surnames = new String[i];
+				double[] salaries = new double[i];
+				int[] ages = new int[i];
+				boolean[] isMarrieds = new boolean[i];
+				boolean[] isFullTimeEmployees = new boolean[i];
+				int[] credits = new int[i];
+
+				
 			System.out.println("Please enter your name:");
-			String name = sc.next();
+			 names[i] = sc.next();	
+			
 			System.out.println("Please enter your surname:");
-			String surname = sc.next();
+			 surnames[i] = sc.next();
 			System.out.println("Please enter your salary in EU:");
-			double salary = sc.nextInt();
+			 salaries[i] = sc.nextInt();
 			System.out.println("How old you are:");
-			int age = sc.nextInt();
+			ages[i] = sc.nextInt();
 			System.out.println("Are you married? (if Yes enter true, if No enter false) ");
-			boolean isMarried = sc.nextBoolean();
+			isMarrieds[i] = sc.nextBoolean();
 			System.out.println("Are you full time employee? (if Yes enter true, if No enter false)");
-			boolean isFullTimeEmployee = sc.nextBoolean();
+			 isFullTimeEmployees[i] = sc.nextBoolean();
 			
 			System.out.println("What credit do you want to apply for?");
 			System.out.println("If you want house loan enter number 1.");
 			System.out.println("If you want cash loan enter number 2");
 			System.out.println("If you want consumer loan enter number 3");
-			int credit = sc.nextInt();
+			 credits[i] = sc.nextInt();
+			 
+					
+				 
+			char quitProgram = 'N';
+			
+			System.out.println("If you want to finish the program press Y, if not press N");
+			quitProgram = sc.next().charAt(0);
+			
+			if (quitProgram ==  'Y' || quitProgram == 'y') {
+				System.exit(0);
+			}	
+			
+		
+			Client clients[] = new Client[i];
+		
+		
 			
 			
-			Client client = new Client(name, surname, salary, age, isMarried, isFullTimeEmployee, credit);
 			
-			if (credit == 1 && client.getAge() < 40 && client.getSalary() > 700 && isMarried == true && isFullTimeEmployee == true) {
-				  System.out.println("Thank you for applying for credit.");
-				  System.out.println("Your personal data are: name and surname -" + client.getName() + " " + client.getSurname() +  ", your salary is - " + client.getSalary() + " eu, you are -" + client.getAge() + " year old.");
-				  System.out.println("Your credit  will be approved!");
-			  } else if (credit == 2 && client.getAge() > 30 && client.getSalary() > 350) {
-				  System.out.println("Thank you for applying for credit.");
-				  System.out.println("Your personal data are: name and surname - " + client.getName() + " " + client.getSurname() +  ", your salary is - " + client.getSalary() + " eu, you are -" + client.getAge() + " year old.");
-				  System.out.println("Your credit  will be approved!");
-			  } else if (credit == 3  && client.getAge() > 20 && client.getSalary() > 250) {
-				  System.out.println("Thank you for applying for credit.");	
-				  System.out.println("Your personal data are: name and surname -" + client.getName() + " " + client.getSurname() +  ", your salary is - " + client.getSalary() + " eu, you are -" + client.getAge() + " year old.");
-				  System.out.println("Your credit  will be approved!");
-			  } else {
-				  System.out.println("Thank you for applying for credit.");
-				  System.out.println("Your personal data are: name and surname -" + client.getName() + " " + client.getSurname() +  ", your salary is - " + client.getSalary() + " eu, you are -" + client.getAge() + " year old.");
-				  System.out.println("Unfortunately, your credit will not be aproved");
-		      }
-			
-			
-		 }
-	 }
+		}
+	}
+ }
 }
+
+
+
