0df60f7b1bdbb5bf356bf397f4039f5a89cebcbd
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-03-05 17:49:31 +0100

    commit

--- alptium-football/src/football/Player.java
+++ alptium-football/src/football/Player.java
@@ -1,5 +1,34 @@
 package football;
 
 public class Player {
-
+	
+	private String name;
+	private String surname;
+	private int age;
+	private String position;
+	
+	
+	public Player(String name, String surname,int age, String position) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.position = position;
+	}	
+	
+	
+		public String getName() {
+			return name;		
+		}
+		
+		public String getSur() {
+			return surname;		
+		}
+		
+		public int getAge() {
+			return age;		
+		}
+		
+		public String getPosition() {
+			return position;		
+		}
 }
