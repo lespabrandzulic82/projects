227f6ebc987a13e6324f63bdbcbae13ede4bb0be
lespabrandzulic82 <branko.spalovic@gmail.com>
2019-02-18 12:01:32 +0100

    commit

---
+++ vezba/src/vezba/TestTax.java
@@ -0,0 +1,29 @@
+package vezba;
+
+
+
+public class TestTax {
+	 
+	 public void main(String[] args) {
+		 
+		 Tax t = new Tax();
+		 t.calcTax();
+			
+		 //System.out.print("tax is" + calcTax);
+		}
+
+}
+ class Tax {
+	 
+	    double grasscome;
+		String state;
+		int dependtents;
+		
+		double calcTax() {
+			
+			return 234.55;
+		}
+		
+	
+}
+ 
